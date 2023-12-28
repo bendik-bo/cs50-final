038222f181de339b8b6daa0a554ce80dc5933ffa
Bendik Kodehode <bendik08.kodehode@gmail.com>
2023-12-28 23:30:45 +0100

    Started work on index and layout, and worked on the groundwork for app.py. Alos added __pycache__ to to .gitignore

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from flask import Flask, redirect, render_template, request, session
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 
+from helpers import login_required
+
 app = Flask(__name__)
 
 # Configure session to use filesystem (instead of signed cookies)
@@ -23,6 +25,15 @@ def after_request(response):
     response.headers["Expires"] = 0
     response.headers["Pragma"] = "no-cache"
     return response
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log user in"""
+    return render_template("login.html")
 
 
 # Close database
