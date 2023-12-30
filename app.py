74e3f8bd71c599e1d69345c893518e11d4c2a9ab
Bendik Kodehode <bendik08.kodehode@gmail.com>
2023-12-30 22:52:22 +0100

    Worked on getting login logic to work. Stuck on keep form when refreshing

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 import sqlite3
 
-from flask import Flask, redirect, render_template, request, session
+from flask import Flask, flash, redirect, render_template, request, session, g
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 
@@ -14,10 +14,6 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-# Database
-connection = sqlite3.connect("database.db")
-db = connection.cursor()
-
 @app.after_request
 def after_request(response):
     """Ensure responses aren't cached"""
@@ -25,6 +21,21 @@ def after_request(response):
     response.headers["Expires"] = 0
     response.headers["Pragma"] = "no-cache"
     return response
+
+# Database
+def get_db():
+    """Establish database connection for request"""
+    if "db" not in g:
+        g.db = sqlite3.connect("database.db")
+        g.db.row_factory = sqlite3.Row
+    return g.db.cursor()
+
+@app.teardown_appcontext
+def close_db(exception):
+    """Close database connection at end of request"""
+    if "db" in g:
+        g.db.close()
+
 
 @app.route("/")
 def index():
@@ -35,6 +46,36 @@ def login():
     """Log user in"""
     return render_template("login.html")
 
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    """Register user"""
+    if request.method == "POST":
+        db = get_db()
 
-# Close database
-connection.close()
+        # Empty username field
+        if not request.form.get("username"):
+            flash("Please fill in a username", "username")
+            return redirect("/signup")
+
+        # Username already taken
+        if db.execute("SELECT * FROM users WHERE username = ?", [request.form.get("username")]).fetchone():
+            flash("Username already exists", "username")
+            return redirect("/signup")
+
+        # Empty password field
+        if not request.form.get("password") or not request.form.get("confirm"):
+            flash("Fill in both password-fields", "password")
+            return redirect("/signup")
+
+        # Password mismatch
+        if request.form.get("password") != request.form.get("confirm"):
+            flash("Passwords do not match", "password")
+            return redirect("/signup")
+
+        # Insert user into database
+        # db.execute("INSERT INTO users (username, hash) VALUES (?, ?)", request.form.get("username"), generate_password_hash(request.form.get("password")))
+
+        return redirect("/")
+
+    else:
+        return render_template("signup.html")
